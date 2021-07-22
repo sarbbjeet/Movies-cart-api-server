336f978a0fa58ceaaa04d7f81e2289ac2abd7f96
Sarbjit Singh <sarbjeets18@gmail.com>
2021-07-23 00:08:26 +0100

    db changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const winston = require('winston')
 const http = require('http')
 const app = express()
 const server = http.createServer(app)
@@ -7,5 +8,11 @@ require('./startup/db')() //mongodb function call
 require('./startup/routes')(app) //all route defined inside routes module
 require('./startup/logging')(app)
 require('./startup/prod')(app) //production environment 
+
+process.on("unhandledRejection", (ex) => {
+    console.error("we got an unhandle rejection")
+    winston.error(ex.message)
+
+})
 const port = process.env.port || 3000;
 server.listen(port, () => console.log(`server lisenting on port=${port }`))
--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose')
 const winston = require('winston')
 const config = require('config')
 module.exports = () => {
-    mongoose.connect(config.get('db'))
+    mongoose.connect(config.get('db'), {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+
+        })
         .then(() => winston.info('connected with mongodb'))
         //.catch(err => console.error(err))
 
