4aeaf6e4764ecc536a55e4159395735d2eaa2fa6
Sarbjit Singh <sarbjeets18@gmail.com>
2021-07-22 22:07:01 +0100

    ready to reploy

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const winston = require('winston')
+const config = require('config')
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/vidly')
+    mongoose.connect(config.get('db'))
         .then(() => winston.info('connected with mongodb'))
         //.catch(err => console.error(err))
 
