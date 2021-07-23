4e442f82b9df2574d86ffcce04e36eb10ceca956
Sarbjit Singh <sarbjeets18@gmail.com>
2021-07-23 17:32:30 +0100

    mailsender added

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ require('./startup/db')() //mongodb function call
 require('./startup/routes')(app) //all route defined inside routes module
 require('./startup/logging')(app)
 require('./startup/prod')(app) //production environment 
+const mailSender = require('./startup/mailSender')
 const route = express.Router()
 
-route.get('/', (req, res) => {
-    res.send("hello world")
+
+route.get('/', async(req, res) => {
+    try {
+
+        const subject = "test mail"
+        const body = `${req.protocol}://${req.get('host')}`
+        const reciever = "sarbjeets18@gmail.com" //receiver email address
+        await mailSender(subject, body, reciever)
+
+    } catch (ex) { return res.status(500).send(ex.message) }
+
+    return res.json({ success: true })
+        //res.send("hello world")
+
+
 })
 process.on("unhandledRejection", (ex) => {
     console.error("we got an unhandle rejection")
