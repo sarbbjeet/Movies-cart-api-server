a929d2249f3e240ebf7691be38b6dadcd263b843
Sarbjit Singh <sarbjeets18@gmail.com>
2021-08-29 22:24:41 +0100

    admin middleware added with movies route

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,5 @@
 //check user is admin or not 
 module.exports = async(req, res, next) => {
-    if (!req.user.admin) return res.status(403).send("Access denied")
-
+    if (!req.user.admin) return res.status(403).json({ success: false, message: "Access denied" })
     next()
 }
--- routes/movies.js
+++ routes/movies.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const { movieSchema, validateMovie, Movie } = require('../module/movie')
 const { Genre } = require('../module/genre')
 const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
 
 route.get('/', async(req, res) => {
     const movies = await Movie.find()
@@ -38,34 +39,34 @@ route.put('/:id', auth, async(req, res) => {
 
 
 route.post('/', auth, async(req, res) => {
-    const { error } = validateMovie(req.body)
-    if (error) return res.status(400).json({ success: false, message: error.details[0].message })
-        //try {
-    const genre = await Genre.findById(req.body.genreId)
-    if (!genre) return res.status(404).json({ success: false, message: "invalid genre..." })
-    const movie = new Movie({
-        title: req.body.title,
-        genre: { //hybrid relational database 
-            _id: req.body.genreId, //reference
-            name: genre.name //embedded 
-        },
-        //genre,    //this code used in embedded document relational database
-        //genre: genre._id  // this code is use in reference based relational database
-        // genre: {    //embedded relational database 
-        //     _id: genre._id,
-        //     name: genre.name,
-        //     desc: genre.desc
-        // },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate
+        const { error } = validateMovie(req.body)
+        if (error) return res.status(400).json({ success: false, message: error.details[0].message })
+            //try {
+        const genre = await Genre.findById(req.body.genreId)
+        if (!genre) return res.status(404).json({ success: false, message: "invalid genre..." })
+        const movie = new Movie({
+            title: req.body.title,
+            genre: { //hybrid relational database 
+                _id: req.body.genreId, //reference
+                name: genre.name //embedded 
+            },
+            //genre,    //this code used in embedded document relational database
+            //genre: genre._id  // this code is use in reference based relational database
+            // genre: {    //embedded relational database 
+            //     _id: genre._id,
+            //     name: genre.name,
+            //     desc: genre.desc
+            // },
+            numberInStock: req.body.numberInStock,
+            dailyRentalRate: req.body.dailyRentalRate
+        })
+        try {
+            const result = await movie.save()
+            res.send(result)
+        } catch (err) { res.status(404).json({ success: false, message: err.message }) } //err.message // error validation
     })
-    try {
-        const result = await movie.save()
-        res.send(result)
-    } catch (err) { res.status(404).json({ success: false, message: err.message }) } //err.message // error validation
-})
-
-route.delete('/:id', auth, async(req, res) => {
+    //auth and admin middlesware added check isAdmin=true or not
+route.delete('/:id', [auth, admin], async(req, res) => {
     let movie = await Movie.findById(req.params.id)
     if (!movie) return res.status(404).json({ success: false, message: 'invaild movie id' })
     return res.json(await Movie.findByIdAndRemove(movie._id)) || ({ success: true, message: `successfully delete ${movie.title}` })
